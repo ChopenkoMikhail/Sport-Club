42c795969a79d9ca7012683407587f7f50db2f7a
ChopenkoMikhail <162743401+ChopenkoMikhail@users.noreply.github.com>
2024-03-09 00:04:05 +0300

    удалил

--- assets/PHP/conect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//    $connect = mysql polzovatili_sport_club.sql < ../SQL/polzovatili_sport_club.sql>
-       $connect = mysqli_connect('localhost', 'root', '', 'polzovatili_sport_club');
-
-    if (!$connect) {
-        die('Error connect to DataBase');
-    }
